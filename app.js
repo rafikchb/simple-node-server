cdcd20b435f461af54147a95b70a1533bb122aac
c22018086 <rafik.chaib@etu.univ-amu.fr>
2023-06-29 13:27:09 +0200

    adding the lunch script

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("hello from les cigales");
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
